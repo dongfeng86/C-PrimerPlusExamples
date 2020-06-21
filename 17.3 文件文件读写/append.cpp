0bafd16f2c7e0f1ff68aed63244a111203523354
dongfeng86 <115949168@qq.com>
2020-06-21 11:28:56 +0800

    2020.6.21 添加了宏重定义的相关内容

--- 17.3 文件文件读写/append.cpp
+++ 17.3 文件文件读写/append.cpp
@@ -2,9 +2,10 @@
 #include<fstream>
 #include<string>
 #include<cstdlib>
+#include <atlstr.h>
 
 
-const char * file = "guests.txt";
+const char * file = "guestsNew22.txt";
 int main()
 {
 	using std::cout;
@@ -14,27 +15,22 @@ int main()
 	using std::ofstream;
 	using std::cerr;
 	using std::string;
-	
+
 	//打开原始文件
-	char ch;
-	//创建输入流对象。只有3步：1.创建一个ifstream对象；2.将其和特定文件关联起来；3.就像使用cin一样使用它。
-	ifstream fin;
+	std::wifstream fin;
+	wchar_t ch;
 	fin.open(file);
-	if (fin.is_open())               //true if the file is open, false otherwise.
+	if (fin.is_open())
 	{
-		cout << "Here are the current contents of the "
-			<< file << " file:\n";
-		while (fin.get(ch))
-			cout << ch;
+		cout << "here are the current contents of the " << file << " file:\n";
+		while (fin.get(ch));
 		fin.close();
 	}
 
-	//在文件中增加新内容
-	//创建输出流对象。只有3步：1.创建一个ofstream对象；2.将其和特定文件关联起来；3.就像使用cout一样使用它。
-	ofstream fout(file, std::ios::out | std::ios::app);       //默认的打开模式为 ios::out | ios::trunc，这将删除文件的原内容
+	std::wofstream fout(file, std::ios::out | std::ios::app);
 	if (!fout.is_open())
 	{
-		cerr << "can't open " << file << " file for output.\n";
+		cerr << "can not open " << file << " file for output.\n";
 		exit(EXIT_FAILURE);
 	}
 
@@ -42,21 +38,52 @@ int main()
 	string name;
 	while (getline(cin, name) && name.size() > 0)
 	{
-		fout << name << endl;
+		fout << CString(CA2T(name.c_str()));
 	}
-	fout.close();
 
-	//显示修改后的文件
-	fin.clear();                     //消除所有错误标志
-	fin.open(file);
-	if (fin.is_open())
-	{
-		cout << "here are the new contents of the "
-			<< file << " file:\n";
-		while (fin.get(ch))
-			cout << ch;
-		fin.close();
-	}
-	cout << "Done.\n";
-	return 0;
+	
+	////打开原始文件
+	//char ch;
+	////创建输入流对象。只有3步：1.创建一个ifstream对象；2.将其和特定文件关联起来；3.就像使用cin一样使用它。
+	//ifstream fin;
+	//fin.open(file);
+	//if (fin.is_open())               //true if the file is open, false otherwise.
+	//{
+	//	cout << "Here are the current contents of the "
+	//		<< file << " file:\n";
+	//	while (fin.get(ch))
+	//		cout << ch;
+	//	fin.close();
+	//}
+
+	////在文件中增加新内容
+	////创建输出流对象。只有3步：1.创建一个ofstream对象；2.将其和特定文件关联起来；3.就像使用cout一样使用它。
+	//ofstream fout(file, std::ios::out | std::ios::app);       //默认的打开模式为 ios::out | ios::trunc，这将删除文件的原内容
+	//if (!fout.is_open())
+	//{
+	//	cerr << "can't open " << file << " file for output.\n";
+	//	exit(EXIT_FAILURE);
+	//}
+
+	//cout << "enter guest names (enter a blank line to quit):\n";
+	//string name;
+	//while (getline(cin, name) && name.size() > 0)
+	//{
+	//	fout << name << endl;
+	//}
+	//fout.close();
+
+	////显示修改后的文件
+	//fin.clear();                     //消除所有错误标志
+	//fin.open(file);
+	//if (fin.is_open())
+	//{
+	//	cout << "here are the new contents of the "
+	//		<< file << " file:\n";
+	//	while (fin.get(ch))
+	//		cout << ch;
+	//	fin.close();
+	//}
+	//cout << "Done.\n";
+	//return 0;
 }
---
+++ 预处理器重定义/预处理器重定义.cpp
@@ -0,0 +1,15 @@
+﻿//这是关于宏重定义一个经典的案例，在预处理阶段，宏并不是按照最后一次
+//定义的数值进行替换，相反，在程序的不同位置，宏均是按照这个位置以前
+//宏的最后一次定义数值进行替换。
+//这和汇编语言中等号伪指令对符号常量的处理一致
+#include<iostream>
+#define _I_COUNT 10
+int main()
+{
+	int iN = _I_COUNT;
+	std::cout << "现在宏的数值为：" << iN << std::endl;	//此时iN为10
+#define _I_COUNT 5
+	int iM = _I_COUNT;
+	std::cout << "现在宏的数值为：" << iM << std::endl;	//此时iM为5
+	return 0;
+}
